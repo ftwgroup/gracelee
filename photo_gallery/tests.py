8b114a8d5c964db8dc617895743072631e098917
Sonya <sonya@brainimac.att.net>
2013-06-10 13:25:52 -0700

    started a new test file

--- photo_gallery/tests.py
+++ photo_gallery/tests.py
@@ -14,3 +14,36 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+####################
+# sudo testing code:
+
+
+class GraceLeeTest(TestCase):
+	#checks to see if imagelist returns any data
+	def test_image_info(self):
+		check_imagelist = imagelist[::]
+		self.assertIsNotNone(imagelist)
+
+	# checks if image:
+	# stores original, stores new size
+	def test_image_resize(self):
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
