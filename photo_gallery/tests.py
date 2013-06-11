ac2b4b014e1599a33e8612e49826ad62989e5048
Sonya <sonya@brainimac.att.net>
2013-06-11 13:18:22 -0700

    changes to test.py

--- photo_gallery/tests.py
+++ photo_gallery/tests.py
@@ -1,34 +1,50 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-
-
-####################
-# sudo testing code:
-
-
 class GraceLeeTest(TestCase):
-	#checks to see if imagelist returns any data
+	# this should set up the pseudo DB
+	def setUp(self):
+		UploadFile.objects.create(image="photo.jpg", comment="first photo")
+		UploadFile.objects.create(image="picture.jpg", comment="second photo")
+		UploadFile.objects.create(image="image.jpg", comment="third photo")
+
+	def test_image_db(self):
+		first = UploadFile.objects.get("photo.jpg")
+		second = UploadFile.objects.get("picture.jpg")
+		third = UploadFile.objects.get("image.jpg")
+		
+		self.assertEqual(first.img_comment(), 'first img')
+		self.assertEqual(second.img_comment(), 'second img')
+		self.assertEqual(third.img_comment(), 'third img')
+
+	# tears the test down
+	def tearDown(self):
+		self.fixture
+
+
+	# checks to see if imagelist returns any data
 	def test_image_info(self):
 		check_imagelist = imagelist[::]
 		self.assertIsNotNone(imagelist)
 
-	# checks if image:
-	# stores original, stores new size
+	# checks if image is stored and resized
 	def test_image_resize(self):
+		# is the images url split?
+		# cropped url set up properly?
+
+		# test static serve request
+		# recieves original image
+		# stores original image
+		# recieves new size
+		# stores new size
+		# returns new image
+
+	def test_image_crop(self):
+		# test static serve request
+		# test is images exist in the DOCUMENT_ROOT
+		# test if new image is cropped
+		# test if cropped image is saved
+		# test is image is returned
 
 
 if __name__ == '__main__':
